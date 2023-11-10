6c581608cc8ab1c49063f3359565685a98725186
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-10 11:21:58 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodo] = useState('')
+  const addTodo = ()=>{
+    setTodos([...todos, newTodo])
+  }
+  const deleteTodo = (index)=>{
+    const filteredTodos = todos.filter((todo,i)=>i!=index)
+    setTodos(filteredTodos)
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <input type="text" onChange={e=>setNewTodo(e.target.value)} />
+        <button onClick={addTodo} >Add Todo</button>
+        <ul>
+          {
+            todos.map((todo, i)=>{
+              return <li>{todo} <button onClick={()=>deleteTodo(i)} >Delete</button></li>
+            })
+          }
+        </ul>
     </div>
   )
 }
