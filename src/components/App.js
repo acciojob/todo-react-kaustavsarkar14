dadbbaa172d5423bc6d91365e8c020914f87d9eb
kaustavsarkar14 <sarkar.kaustav14@gmail.com>
2023-11-14 19:27:27 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,49 @@
-
-import React,{useState} from "react";
+import React, { useState } from 'react';
 import './../styles/App.css';
 
 const App = () => {
-  const [todos, setTodos] = useState([])
-  const [newTodo, setNewTodo] = useState('')
-  const addTodo = ()=>{
-    setTodos([...todos, newTodo])
-    setNewTodo('')
-  }
-  const deleteTodo = (index)=>{
-    const filteredTodos = todos.filter((todo,i)=>i!=index)
-    setTodos(filteredTodos)
-  }
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo('');
+  };
+
+  const handleDelete = (index) => {
+    setTodos(todos.filter((todo, i) => i !== index));
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-        <input type="text" value={newTodo} onChange={e=>setNewTodo(e.target.value)} />
-        <button onClick={addTodo} >Add Todo</button>
-        <ul>
-          {
-            todos.map((todo, i)=>{
-              return <li>{todo} <button onClick={()=>deleteTodo(i)} ></button></li>
-            })
-          }
-        </ul>
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <h1>To-Do List</h1>
+      <form>
+        <input
+          type="text"
+          id="todo"
+          name="todo"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <button onClick={handleAdd}>Add Todo</button>
+      </form>
 
-export default App
+      <ul>
+        {todos.map((todo, index) => {
+          return (
+            <li key={index}>
+              <div className="todo-item">
+                <p>{todo}</p>
+                <button onClick={() => handleDelete(index)}></button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
